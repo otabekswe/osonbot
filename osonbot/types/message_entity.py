a6681840c772a6c28b48c8aebe37aa36845354d0
Otabek Nurmatov <otabekswe@Mac.local>
2023-07-10 00:36:31 +0500

    UPDATED: added Chat Actions

--- osonbot/types/message_entity.py
+++ osonbot/types/message_entity.py
@@ -42,3 +42,16 @@ class MessageEntityType:
     PRE = 'pre'
     TEXT_LINK = 'text_link'
     TEXT_MENTION = 'text_mention'
+
+
+class ChatAction:
+    TYPING = 'typing'
+    UPLOAD_PHOTO = 'upload_photo'
+    RECORD_VIDEO = 'record_video'
+    UPLOAD_VIDEO = 'upload_video'
+    RECORD_AUDIO = 'record_audio'
+    UPLOAD_AUDIO = 'upload_audio'
+    UPLOAD_DOCUMENT = 'upload_document'
+    FIND_LOCATION = 'find_location'
+    RECORD_VIDEO_NOTE = 'record_video_note'
+    UPLOAD_VIDEO_NOTE = 'upload_video_note'
